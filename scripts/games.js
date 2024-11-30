71befe5787e956195449d44fa162d283ece35f2b
lgandarillas <lgandaf@gmail.com>
2024-11-30 20:02:17 +0100

    Add: game 1

--- scripts/games.js
+++ scripts/games.js
@@ -1,14 +1,27 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const gameButtons = document.querySelectorAll('.game_btn');
     const closeGameButton = document.querySelector('.close-game_btn');
     const gameContainer = document.querySelector('.game_container');
     const gameButtonsContainer = document.querySelector('.game_buttons');
+    const games = document.querySelectorAll('.game');
 
     // Mostrar el juego seleccionado
     function showGame(gameNumber) {
+        // Ocultar todos los juegos
+        games.forEach(game => game.classList.remove('show'));
+
+        // Mostrar el juego seleccionado
+        const selectedGame = document.getElementById(`game${gameNumber}`);
+        if (selectedGame) {
+            selectedGame.classList.add('show');
+        }
+
+        // Mostrar el contenedor del juego y el botón de cerrar
         gameContainer.style.display = 'flex';
-        gameContainer.innerHTML = `<h2>Juego ${gameNumber}</h2>`;
         closeGameButton.style.display = 'flex';
+
+        // Ajustar el diseño de los botones
         gameButtonsContainer.classList.add('open');
     }
 
@@ -22,10 +35,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Función para cerrar el juego
     function closeGame() {
+        // Ocultar todos los juegos
+        games.forEach(game => game.classList.remove('show'));
+
+        // Ocultar el contenedor del juego y el botón de cerrar
         gameContainer.style.display = 'none';
-        gameContainer.innerHTML = ''; 
-        closeGameButton.style.display = 'none'; 
+        closeGameButton.style.display = 'none';
+
+        // Restablecer el diseño de los botones
         gameButtonsContainer.classList.remove('open');
+
+        resetGameState();
+    }
+
+    // Restablecer el estado del juego
+    function resetGameState() {
+        games.forEach(game => {
+            const placedAccessories = game.querySelectorAll('.placed-accessory');
+            placedAccessories.forEach(accessory => {
+                accessory.remove(); // Eliminar los accesorios colocados
+            });
+        });
     }
 
     // Cerrar el juego
