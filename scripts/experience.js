dcfb614cdb164256036a5068d2760593779fb002
CarlosBravoGarran <carlosbravogarran@gmail.com>
2024-12-02 20:31:30 +0100

    Add: link to experience book

---
+++ scripts/experience.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.experience_registration_button').addEventListener('click', function() {
+        window.location.href = '/experience.html';
+    });
+});
